3dcf336a6bf19a41f4d2ae62a914fb0c63cdd6bb
Bramanda Suwandi <bramanda48@gmail.com>
2023-12-03 08:58:12 +0700

    feat(layout): add base header and footer template

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,135 @@
+<header class="navbar navbar-expand-md d-print-none" data-bs-theme="dark">
+    <div class="container-xl">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
+            aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+            <a href="." class="hover:no-underline">
+                Docker Mailserver Manager
+            </a>
+        </h1>
+        <div class="navbar-nav flex-row order-md-last">
+            <div class="nav-item d-none d-md-flex me-3">
+                <div class="btn-list">
+                    <a href="https://github.com/tabler/tabler" class="btn" target="_blank" rel="noreferrer">
+                        <i class="ti ti-brand-github text-lg leading-none mr-1"></i>
+                        Source code
+                    </a>
+                    <a href="https://github.com/sponsors/codecalm" class="btn" target="_blank" rel="noreferrer">
+                        <i class="ti ti-heart text-lg leading-none mr-1"></i>
+                        Sponsor
+                    </a>
+                </div>
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
+                    aria-label="Open user menu">
+                    <span class="avatar avatar-sm" style="background-image: url(./user.svg)"></span>
+                    <div class="d-none d-xl-block ps-2">
+                        <div>John Doe</div>
+                        <div class="mt-1 small text-muted">Administrator</div>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                    <a href="#" class="dropdown-item">Status</a>
+                    <a href="./profile.html" class="dropdown-item">Profile</a>
+                    <a href="#" class="dropdown-item">Feedback</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="./settings.html" class="dropdown-item">Settings</a>
+                    <a href="./sign-in.html" class="dropdown-item">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<header class="navbar-expand-md">
+    <div class="navbar-collapse" id="navbar-menu">
+        <div class="navbar">
+            <div class="container-xl">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-home text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-world-www text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Domains
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-shield-check text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                SSL Certificates
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./form-elements.html">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-users text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Accounts
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./icons.html">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-route-alt-right text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Mail Forwarding
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./icons.html">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-affiliate text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Relay
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./icons.html">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-book text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Audit Log
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./icons.html">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-settings text-xl leading-none"></i>
+                            </span>
+                            <span class="nav-link-title">
+                                Settings
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
