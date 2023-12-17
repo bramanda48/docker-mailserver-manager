6c3470360fe597f23781b14db37a445ddc721ab1
Bramanda Suwandi <bramanda48@gmail.com>
2023-12-17 23:07:22 +0700

    feat(layout): disable preflight and add css label error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  corePlugins: {
+    preflight: false,
+  },
   content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue"],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: ["error"],
 };
